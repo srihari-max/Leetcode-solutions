7eff521f1b17c8ae44a0d3448cc1a6a4803a6b07
sri hari <sriharinarayanan06@gmail.com>
2026-06-21 18:17:50 +0530

    Add leetcode 1480

---
+++ 1480_Running_sum_of_1D_array.py
@@ -0,0 +1,9 @@
+class Solution:
+    def runningSum(self, nums: List[int]) -> List[int]:
+        res=[]
+        s=0
+        for i in range(len(nums)):
+            s+=nums[i]
+            res.append(s)
+        return res
+        
